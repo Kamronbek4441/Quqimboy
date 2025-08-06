1f0ad343aec4a2984572c9a6d4ae6119a065381a
unknown <kamronbekdilshadov24@gmail.com>
2025-08-06 23:10:14 +0500

    master

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,41 +1,41 @@
 import React from "react";
 import "./Products.css";
-import joja from "../../assets/chip.svg"
-import back from "../../assets/Background.svg"
-import chip from "../../assets/jojachip.svg"
-import broiler from "../../assets/broiler.svg"
-import border from "../../assets/Border.svg"
-import yem from "../../assets/yem.svg"
+import joja from "../../assets/chip.svg";
+import back from "../../assets/Background.svg";
+import chip from "../../assets/jojachip.svg";
+import broiler from "../../assets/broiler.svg";
+import border from "../../assets/Border.svg";
+import yem from "../../assets/yem.svg";
 
 const Products = () => {
   return (
-    <section class="products-section">
-      <div class="header">
-        <img src={joja} alt="" />
+    <div className="products">
+      <div className="prod1" style={{
+        backgroundImage: `url(${joja})`,
+        width: "400px",
+      }}>
+        <h1>Bizning Mahsulotlarimiz</h1>      
       </div>
-
-      <div className="back"  style={{
+      <div className="prod2" style={{
         backgroundImage: `url(${back})`,
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
-        backgroundSize: "cover"
+        backgroundSize: "contain"
       }}>
-        <div class="products-grid">
-        <div class="product-card">
-          <img src={chip} alt="Chicks in Eggs" />
+        <div className="pro">
+          <img src={chip} alt="" />
         </div>
-        <div class="product-card">
-          <img src={border} alt="Chicks Group" />
+        <div className="pro">
+          <img src={border} alt="" />
         </div>
-        <div class="product-card">
-          <img src={broiler} alt="Broiler Meat" />
+        <div className="pro">
+          <img src={broiler} alt="" />
         </div>
-        <div class="product-card">
-          <img src={yem} alt="Feed Bags" />
+        <div className="pro">
+          <img src={yem} alt="" />
         </div>
       </div>
-      </div>
-    </section>
+    </div>    
   );
 };
 
