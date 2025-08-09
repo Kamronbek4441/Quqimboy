e5b3df6c146af7674db95cad851b86dce5b8b6fb
unknown <kamronbekdilshadov24@gmail.com>
2025-08-09 11:21:09 +0500

    added image to slide

--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -6,12 +6,17 @@ import phoneIcon from "../../assets/telephone.png";
 import juires from "../../assets/juires.svg";
 import can from "../../assets/can.svg";
 import file from "../../assets/file.svg";
+import slide from "../../assets/slide.jpg";
+import ros from "../../assets/ros.jpg";
 import slide1 from "../../assets/slide1.jpg";
 import slide2 from "../../assets/slide2.jpg";
 import slide3 from "../../assets/slide3.jpg";
 import slide4 from "../../assets/slide4.jpg";
+import slide5 from "../../assets/slide5.jpg";
+import slide6 from "../../assets/slide6.jpg";
+import slide7 from "../../assets/slide7.jpg";
 
-const images = [slide1, slide2, slide3, slide4]; // ✅ Import qilingan rasmlarni ishlatyapmiz
+const images = [slide, slide1, ros, slide2, slide3, slide7, slide4, slide5, slide6];
 
 function Hero() {
   const [index, setIndex] = useState(0);
